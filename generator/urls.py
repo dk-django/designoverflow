844f641adc927922c91dc4bc85c9cdc59930f3b2
dfemne <udofaemmanuel51@gmail.com>
2024-02-17 07:53:18 +0100

    urls an dviews all set

---
+++ generator/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+from .views import HomepageView
+
+urlpatterns = [
+    path('home/', HomepageView.as_view(), name='home-page'),
+]
--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import TemplateView 
+
+
+class HomepageView(TemplateView):
+    template_name = 'generator/homepage.html'
